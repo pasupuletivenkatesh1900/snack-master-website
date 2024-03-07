ee34689304c432f8eedc5912c65126977de0bd2c
Venkatesh <pasupuletivenkatesh1900@gmail.com>
2024-03-07 23:38:14 +0530

    navbar demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import Navbar from './components/Navbar.js';
 function App() {
   return (
+    <>
+    <Navbar />
     <h1>Snack Master</h1>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function Navbar (){
+    return(
+        <nav>
+      <ul>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    )
+}
+
+export default Navbar;
