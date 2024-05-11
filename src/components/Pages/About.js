7c40ee3fc0149c95250c1351e515b84f04a9fe5b
pasupuletivenkatesh1900 <pasupuletivenkatesh1900@gmail.com>
2024-05-11 13:45:22 +0530

    about

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import CEOImage from '../../images/about/ceo.jpg'; // Import the CEO image
-import '../../styles/pages/About.css';
+import React from "react";
+import CEOImage from "../../images/about/ceo.jpg"; // Import the CEO image
+import "../../styles/pages/About.css";
 
 function About() {
-    return (
-        <div className="about-container">
-            <div className="ceo-info">
-                <img src={CEOImage} alt="CEO" className="ceo-image" />
-                <div className="ceo-description">
-                    <h2>CEO Name</h2>
-                    <p>CEO Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Phone: +1234567890</p>
-                    <p>Email: ceo@example.com</p>
-                </div>
-            </div>
+  return (
+    <div className="about-container">
+      <div className="ceo-info">
+        <img src={CEOImage} alt="CEO" className="ceo-image" />
+        <div className="ceo-description">
+          <h2>CEO Venkatesh</h2>
+          <p>
+            CEO Description: Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua.
+          </p>
+          <p>Phone: +1234567890</p>
+          <p>Email: ceo@example.com</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default About;
